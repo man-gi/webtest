7cfe771ded11d3695a1ee33ccf92097a56fd3895
man-gi <aksrl2639@gmail.com>
2021-12-05 19:58:42 +0900

    commit

--- nodejs/mysql.js
+++ nodejs/mysql.js
@@ -1,10 +1,10 @@
 var mysql      = require('mysql');
 // 비밀번호는 별도의 파일로 분리해서 버전관리에 포함시키지 않아야 합니다. 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '111111',
-  database : 'opentutorials'
+  host     : 'ik1eybdutgxsm0lo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user     : 'ge71in1m60udiqdi',
+  password : 'qtxf3yb01ht64igw',
+  database : 'b7ytg0pl88aatm9d'
 });
  
 connection.connect();
